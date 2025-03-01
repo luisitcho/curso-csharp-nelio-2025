85a41be371bfa092a14b8e5426b4b0ec6487c940
luisitcho <luishenrisc1@gmail.com>
2025-03-01 18:18:21 -0300

    curso c# - aula 1

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); // Jeito novo de escrever
+
+
+// Jeito antigo de escrever
+//using System;
+
+//namespace Aula1 {
+//    internal class Program {
+//        static void Main(string[] args) {
+//            Console.WriteLine("Hello World!");
+//        }
+//    }
+//}
