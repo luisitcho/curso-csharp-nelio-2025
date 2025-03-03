4b832f609b541b146a30e5481150fcb47d66842d
luisitcho <luishenrisc1@gmail.com>
2025-03-03 06:20:36 -0300

    curso c# - aula 6 - estrutura de repetição (for)

---
+++ Aula6/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Quantos números inteiros você vai digitar? ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++) {
+            Console.Write("Valor #{0}: ", i);
+            int x = int.Parse(Console.ReadLine());
+        }
+    }
+}
