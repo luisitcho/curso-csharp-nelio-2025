6de8c6119168103b40a65333db1edfc6c1679f22
luisitcho <luishenrisc1@gmail.com>
2025-03-02 22:37:07 -0300

    curso c# - aula 3 - entrada de dados

---
+++ Aula3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+class Program {
+    static void Main() {
+        //string frase = Console.ReadLine();
+        //string x = Console.ReadLine();
+        //string y = Console.ReadLine();
+        //string z = Console.ReadLine();
+      
+        string[] v = Console.ReadLine().Split(' ');
+        string a = v[0];
+        string b = v[1];
+        string c = v[2];
+
+        //Console.WriteLine($"Você digitou na frase: {frase}");
+        //Console.WriteLine($"Você digitou no X: {x}");
+        //Console.WriteLine($"Você digitou no Y: {y}");
+        //Console.WriteLine($"Você digitou no Z: {z}");
+
+        Console.WriteLine($"Você digitou no A: {a}");
+        Console.WriteLine($"Você digitou no B: {b}");
+        Console.WriteLine($"Você digitou no C: {c}");
+
+    }
+}
