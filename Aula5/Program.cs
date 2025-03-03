00071cc5a363fcc78e9325f4304c4168eb5cc5de
luisitcho <luishenrisc1@gmail.com>
2025-03-03 05:54:38 -0300

    curso c# - aula 3 - estrutura de repetição (while)

---
+++ Aula5/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Globalization;
+
+class Program {
+    static void Main() {
+
+        Console.WriteLine("Enter a number: ");
+        double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        while (x >= 0) {
+            double squareRoot = Math.Sqrt(x);
+            Console.WriteLine(squareRoot.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Enter another number: ");
+            x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        Console.WriteLine("Negative number!");
+    }
+}
