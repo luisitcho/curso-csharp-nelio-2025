58c3d7a11f7e689e5f5e03bbf18edcc3ad4719f4
luisitcho <luishenrisc1@gmail.com>
2025-03-02 22:56:05 -0300

    curso c# - aula 2

--- Aula2/Aula2/obj/Debug/net8.0/Aula2.AssemblyInfo.cs
+++ Aula2/Aula2/obj/Debug/net8.0/Aula2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd8abdf813ea38448493282904e0c8f8155faeb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461b9609ea87435583872c73d43b00f90e1cdeb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
